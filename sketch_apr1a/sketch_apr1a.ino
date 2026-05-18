3f0388e46857b22b1231fa9d8426b7d79e10b473
dg015 <bruno.barbosa8@yahoo.com>
2026-05-18 15:10:51 -0400

    colour detection works now

--- sketch_apr1a/sketch_apr1a.ino
+++ sketch_apr1a/sketch_apr1a.ino
@@ -33,6 +33,15 @@ float coasterValue3;
 #define LDR_PIN 2
 
 
+// colour detection
+#define s0 3
+#define s1 4
+#define s2 5
+#define s3 6
+#define out 7
+int   Red=0, Blue=0, Green=0;  //RGB values 
+
+
 void setup()
 {
   Serial.begin(115200);
@@ -43,28 +52,106 @@ void setup()
   // beer tap
   pinMode(switchPin, INPUT_PULLUP);
 
+  //colour detection pin setup
+  pinMode(s0,OUTPUT);    
+  pinMode(s1,OUTPUT);
+  pinMode(s2,OUTPUT);
+  pinMode(s3,OUTPUT);
+  pinMode(out,INPUT);
+
+  digitalWrite(s0,HIGH);
+
   //RFID card
   SPI.begin();      // Initiate  SPI bus
- // mfrc522.PCD_Init();   // Initiate MFRC522
+  mfrc522.PCD_Init();   // Initiate MFRC522
   Serial.println("Approximate your card to the reader...");
-
 }
 
 
 
 void loop()
 {
-
-
   //checkButton();
   //checkSwitchState();
-  readCard();
+  //readCard();
 
   //coaster
-  checkLightCoaster(A0,coasterValue1);
-  checkLightCoaster(A1,coasterValue2);
-  checkLightCoaster(A2,coasterValue3);
+  //checkLightCoaster(A0,coasterValue1);
+  //checkLightCoaster(A1,coasterValue2);
+  //checkLightCoaster(A2,coasterValue3);
+  getColour();
 
+}
+
+void getColour()
+{
+  /*
+    digitalWrite(s2,LOW);        //S2/S3  levels define which set of photodiodes we are using LOW/LOW is for RED LOW/HIGH  is for Blue and HIGH/HIGH is for green
+   digitalWrite(s3,LOW);
+   Serial.print("Red  value= "); 
+   GetColourData();                   //Executing GetData function to  get the value
+
+   digitalWrite(s2,LOW);
+   digitalWrite(s3,HIGH);
+   Serial.print("Blue  value= ");
+   GetColourData();
+
+   digitalWrite(s2,HIGH);
+   digitalWrite(s3,HIGH);
+   Serial.print("Green value= ");
+   GetColourData();
+
+   Serial.println();
+
+   delay(2000);
+  */
+    GetColors();                                     //Execute the GetColors function   to get the value of each RGB color
+                                                   //Depending   of the RGB values given by the sensor we can define the color and displays it on   the monitor
+
+  if (Red <=15 && Green <=15 && Blue <=15)         //If the values   are low it's likely the white color (all the colors are present)
+      Serial.println("White");                     
+      
+  else if (Red<Blue && Red<=Green && Red<23)      //if   Red value is the lowest one and smaller thant 23 it's likely Red
+      Serial.println("Red");
+
+   else if (Blue<Green && Blue<Red && Blue<20)    //Same thing for Blue
+      Serial.println("Blue");
+
+   else if (Green<Red && Green-Blue<= 8)           //Green it was a little tricky,   you can do it using the same method as above (the lowest), but here I used a reflective   object
+      Serial.println("Green");                    //which means the   blue value is very low too, so I decided to check the difference between green and   blue and see if it's acceptable
+
+  else
+     Serial.println("Unknown");                   //if the color is not recognized, you can add as many as you want
+
+
+   delay(2000);                                   //2s delay you can modify if you   want
+  
+
+}
+
+/*
+void GetColourData(){
+
+   data=pulseIn(out,LOW);       //here  we wait until "out" go LOW, we start measuring the duration and stops when "out"  is HIGH again
+   Serial.print(data);          //it's a time duration measured,  which is related to frequency as the sensor gives a frequency depending on the color
+   Serial.print("\	");          //The higher the frequency the lower the duration
+   delay(20);
+
+}
+*/
+
+void GetColors()  
+{    
+  digitalWrite(s2,   LOW);                                           //S2/S3 levels define which set   of photodiodes we are using LOW/LOW is for RED LOW/HIGH is for Blue and HIGH/HIGH   is for green 
+  digitalWrite(s3, LOW);                                           
+   Red = pulseIn(out, digitalRead(out) == HIGH ? LOW : HIGH);       //here we wait   until "out" go LOW, we start measuring the duration and stops when "out" is   HIGH again, if you have trouble with this expression check the bottom of the code
+   delay(20);  
+  digitalWrite(s3, HIGH);                                         //Here   we select the other color (set of photodiodes) and measure the other colors value   using the same techinque
+  Blue = pulseIn(out, digitalRead(out) == HIGH ? LOW   : HIGH);
+  delay(20);  
+  digitalWrite(s2, HIGH);  
+  Green = pulseIn(out,   digitalRead(out) == HIGH ? LOW : HIGH);
+  delay(20);  
 }
 
 void readCard()
@@ -110,18 +197,29 @@ void readCard()
 
 void checkLightCoaster(int pin, float &coasterValue)
 {
+
+
+
+  /*
+    int analogValue = analogRead(pin);
+  float voltage = analogValue / 1024. * 5;
+  float resistance = 2000 * voltage / (1 - voltage / 5);
+  float lux = pow(RL10 * 1e3 * pow(10, GAMMA) / resistance, (1 / GAMMA));
+  Serial.println(lux);
+  */
   int analogValue = analogRead(pin);
   float voltage = analogValue / 1024. * 5;
   float resistance = 2000 * voltage / (1 - voltage / 5);
   float lux = pow(RL10 * 1e3 * pow(10, GAMMA) / resistance, (1 / GAMMA));
   Serial.println(lux);
+
+
 }
 
 
 //will pass the data to the arduino
 void gatherData()
 {
-
 
 
 }
